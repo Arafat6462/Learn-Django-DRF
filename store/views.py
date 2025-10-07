92db7ad3829849268d470e9d6a983a761222de57
Arafat Hossain <arafat6462@gmail.com>
2025-10-07 22:23:28 +0600

    Add API endpoint for product list view using Django REST Framework

--- store/views.py
+++ store/views.py
@@ -9,5 +9,9 @@ In Django, there are two main classes for handling HTTP communication: `HttpRequ
 The `HttpRequest` class represents the incoming request from the client, while the `HttpResponse` class is used to construct the response sent back to the client.
 Note: Django REST Framework (DRF) introduces its own `Request` and `Response` classes, which provide additional features for building RESTful APIs, such as content negotiation and more flexible data handling.
 """
+@api_view() # DRF decorator to specify that this view is an API endpoint so it can handle various HTTP methods like GET, POST, etc.
 def product_list(request):
-    return HttpResponse("This is a test view from store app.")
+    # return HttpResponse("OK") # Basic Django response. this is not suitable for APIs. 
+    return Response("OK") # DRF response, suitable for APIs. it can handle various data formats like JSON, XML, etc.
+# Note: only two changes are made here. 1. @api_view() decorator is added. 2. HttpResponse is replaced with Response. with these two changes, this view function is now ready to serve as an API endpoint.
+# This two changes also makes browserable API. you can visit this endpoint in browser and see the response in a nice format. 
