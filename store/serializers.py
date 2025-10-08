8eaf4bd983402d7e59160e258c60d7b85057ce5b
Arafat Hossain <arafat6462@gmail.com>
2025-10-08 14:24:37 +0600

    Add price field to ProductSerializer and implement price_with_tax calculation

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 from rest_framework import serializers
+from store.models import Product
 
 # DRF serializers are responsible for transforming complex data (like Django models) into native Python datatypes. This makes it easy to render data as JSON, XML, etc.
 # Serializers also handle deserialization: they validate and transform incoming data (such as JSON from an API request) back into Python objects or Django models.
@@ -23,4 +25,8 @@ from rest_framework import serializers
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255) # we add all this fields manually again because when user sends data to create a new product, we need to validate these fields and convert them to python datatypes
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2) # name of the field in the model is unit_price but we can to expose it as price in the API. so we can change the name here. name does not have to be same as model field name.
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price') # name of the field in the model is unit_price but we can to expose it as price in the API. so we can change the name here. name does not have to be same as model field name. here source='unit_price' tells the serializer to use the unit_price field from the model for this price field.
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax') # this field is not in the model. we are adding this field to the serializer only. SerializerMethodField is a read-only field that gets its value by calling a method on the serializer class. method_name specifies the name of the method to call to get the value for this field.
+
+    def calculate_tax(self, product: Product): # Here :Product is a type hint indicating that the product parameter should be an instance of the Product model. this helps with code readability and can assist IDEs in providing better autocompletion and type checking.
+        return product.unit_price * Decimal(1.1) # Decimal is used to avoid floating point precision issues.
