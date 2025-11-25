feec856c078429dcce392fc580242b590ea5f09e
Arafat Hossain <arafat6462@gmail.com>
2025-11-25 14:52:12 +0600

    Add access token lifetime configuration for JWT authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -167,6 +168,7 @@ REST_FRAMEWORK = {
 # This will set the prefix the token type in the Authorization header
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME':  timedelta(days=1),  # set token lifetime as needed
 }
 
 AUTH_USER_MODEL = 'core.User'  # specify the custom user model
